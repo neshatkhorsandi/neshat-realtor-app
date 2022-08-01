8a916f36b75b93b10315f781c127386c16aa680d
Neshat Khorsandi <neshat.khorsandi@edu.sait.ca>
2022-08-01 11:50:31 -0600

    created fetchApi file

---
+++ utils/fetchApi.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const baseUrl = 'https://bayut.p.rapidapi.com';
+
+export const fetchApi = async (url) => {
+  const { data } = await axios.get(url, {
+    headers: {
+      'X-RapidAPI-Key': '3a75ada366mshc09c85d4cd64a10p1df2b9jsn7c8c8a9c70cf',
+      'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
+    },
+  });
+};
