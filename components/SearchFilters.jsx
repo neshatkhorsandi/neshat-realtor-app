d94117002f777f9c2d2ca4f525a96396c00b1b6e
Neshat Khorsandi <neshat.khorsandi@edu.sait.ca>
2022-08-02 15:49:53 -0600

    created SearchFilters component

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { MdCancel } from 'react-icons/md';
+import Image from 'next/image';
+import {
+  Flex,
+  Select,
+  Box,
+  Text,
+  Input,
+  Spinner,
+  Icon,
+  Button,
+} from '@chakra-ui/react';
+
+import React from 'react';
+
+const SearchFilters = () => {
+  return <div>SearchFilters</div>;
+};
+
+export default SearchFilters;
---
+++ pages/search.js
@@ -0,0 +1,34 @@
+import { Flex, Box, Text, Icon } from '@chakra-ui/react';
+import { BsFilter } from 'react-icons/bs';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import SearchFilters from '../components/SearchFilters';
+
+const Search = () => {
+  const [searchFilters, setSearchFilters] = useState(false);
+  const router = useRouter();
+  return (
+    <Box>
+      <Flex
+        cursor='pointer'
+        bg='gray.100'
+        borderBottom='1px'
+        borderColor='gray.200'
+        p='2'
+        fontWeight='black'
+        fontSize='lg'
+        alignItems='center'
+        justifyContent='center'
+        onClick={() => setSearchFilters(!searchFilters)}
+      >
+        <Text>Search Property By Filters</Text>
+        <Icon paddingLeft='2' w='7' as={BsFilter} />
+      </Flex>
+
+      {searchFilters && <SearchFilters />}
+    </Box>
+  );
+};
+
+export default Search;
