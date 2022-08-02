9a26d2cf29c8c3248749c422b5d47d8035bbb172
Neshat Khorsandi <neshat.khorsandi@edu.sait.ca>
2022-08-02 15:17:16 -0600

    created Layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import Router from 'next/router';
+import Head from 'next/head';
+import NProgress from 'nprogress';
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
